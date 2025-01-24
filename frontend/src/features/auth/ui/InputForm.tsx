51f195f1b7ca9289be67d9e3c82654c8a6a84f40
Thelmo-SM <toraphebi@gmail.com>
2025-01-23 20:59:30 -0400

    ...

--- frontend/src/features/auth/ui/InputForm.tsx
+++ frontend/src/features/auth/ui/InputForm.tsx
@@ -6,7 +6,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 export const InputForm = (props: Props) => {
     return (
         <input
-            className="bg-p-basico w-full my-[0.5rem] h-[2rem] pl-8 rounded-[.2rem] py-[1.5rem] md:py-[1rem]"
+            className="bg-p-basico w-full my-[0.5rem] h-[2rem] pl-8 rounded-[.2rem] py-[1rem]"
             {...props}
         />
     );
