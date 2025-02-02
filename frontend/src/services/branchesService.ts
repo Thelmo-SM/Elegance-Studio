a61c1ff49bdeeff0a00dd65faecb1ad02bdc5f66
Thelmo-SM <toraphebi@gmail.com>
2025-02-02 12:40:28 -0400

    ...

---
+++ frontend/src/services/branchesService.ts
@@ -0,0 +1,42 @@
+import { brachesTypes } from "@/types/branchesTypes";
+import { useState, useEffect } from "react";
+import {collection, getDocs} from 'firebase/firestore'
+import { db } from "@/utils/firebase";
+
+
+export const BranchesService = () => {
+    const [branch, setBrach] = useState<brachesTypes[]>([]);
+
+
+    useEffect(() => {
+            const fechBranch = async () => {
+                try {
+                    const branchRef = collection(db, 'branches');
+                    const branchData = await getDocs(branchRef);
+
+                    const branchesList = branchData.docs.map((doc) => {
+                        const data = doc.data();
+
+                        return {
+                            id: doc.id,
+                            name: data.name || '',
+                            coordinates: data.coordinates || [0, 0],
+                            description: data.description || '',
+                            phone: data.phone || ''
+                        }
+                    });
+
+                    setBrach(branchesList);
+                    
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+            fechBranch()
+    }, []);
+    
+
+    return {
+        branch
+    }
+};
