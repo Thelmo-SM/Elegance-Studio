f67d48622656c44cf970a2d76d1a2a95f953d26e
Thelmo-SM <toraphebi@gmail.com>
2025-02-14 20:24:09 -0400

    Solucionando problemas con vercel-2

--- frontend/lib/firebase.admin.ts
+++ frontend/lib/firebase.admin.ts
@@ -1,11 +1,10 @@
-import admin from 'firebase-admin';
-
+import * as admin from "firebase-admin";
 const firebaseAdminConfig = process.env.FIREBASE_ADMIN_KEY
   ? JSON.parse(process.env.FIREBASE_ADMIN_KEY)
   : null;
 
 if (!firebaseAdminConfig || !firebaseAdminConfig.project_id) {
-  throw new Error('Firebase service account is missing or invalid');
+  throw new Error("Firebase service account is missing or invalid");
 }
 
 if (!admin.apps.length) {
@@ -14,4 +13,5 @@ if (!admin.apps.length) {
   });
 }
 
-export default admin;
+export const authAdmin = admin.auth();
+export const dbAdmin = admin.firestore();
